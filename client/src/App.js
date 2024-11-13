fd1ad9327303127a9ad13f1d8d9be084544d2b3d
Mina Soltani <112659315+MinaSoltani70@users.noreply.github.com>
2024-11-13 16:07:44 +0200

    Landing page frontend v0

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <hr className="header-separator" />
+      <section className="section1">
+        <img
+          className="section1_img"
+          src="https://picsum.photos/1920/600" // the first big image on the home page, will be later replaced by information from the open source API
+          alt="Movies in theaters now"
+        />
+        <h2 className="section1_header">In Theaters Now</h2>
+      </section>
+      <main>
+        <h2>Eyeland; your destination for film discoveries</h2>
+      </main>
+      <Footer />
     </div>
   );
 }
