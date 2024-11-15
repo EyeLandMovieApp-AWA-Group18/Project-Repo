ede15ae3a3badf38a8e68ad767e20976c42eed4e
Mina Soltani <112659315+MinaSoltani70@users.noreply.github.com>
2024-11-15 10:06:07 +0200

    Debugged App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Profile from './pages/Profile.js';
 import MovieDetail from './pages/moiveDetail.js';
 import Authentication from './Authentication.js';
 import RemoveAccount from './pages/RemoveAccount.js';
+import SearchPage from './pages/searchPage.js';
 import './App.css';
 
   
@@ -24,38 +25,16 @@ const App = () => {
           <Route path="/moviedetail" element={<MovieDetail />} />
           <Route path="/auth" element={<Authentication />} />
           <Route path="/removeaccount" element={<RemoveAccount />} /> 
-          {/* Add more routes for other pages */}
+          <Route path="/auth" element={<Authentication />} />
+          <Route path="/search" element={<SearchPage />} />
         </Routes>
 
         {/* Conditionally render Footer only on the Home page */}
         {window.location.pathname === "/" && <Footer />}
       </Router>
     </div>
-import SearchPage from './pages/searchPage.js';
-
-const App = () => {
-  return (
-    <Router>
-      <Header />
-      <hr className="header-separator" />
-      <section className="section1">
-        <img
-          className="section1_img"
-          src="https://picsum.photos/1920/600" // Placeholder image; replace with information from the open source API
-          alt="Movies in theaters now"
-        />
-        <h2 className="section1_header">In Theaters Now</h2>
-      </section>
-      <main>
-        <h2>Eyeland: your destination for film discoveries</h2>
-      </main>
-      <Routes>
-        <Route path="/auth" element={<Authentication />} />
-        <Route path="/search" element={<SearchPage />} />
-      </Routes>
-      <Footer />
-    </Router>
   );
-};
+}
+
 
 export default App;
