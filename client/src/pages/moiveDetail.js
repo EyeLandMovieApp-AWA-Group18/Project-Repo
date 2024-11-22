d7b8a60864a60235ac3fd86c2fb9ccfda4b350dd
PengfeiLi-OAMK <t3lipe01@students.oamk.fi>
2024-11-23 00:38:19 +0200

    Add favorite button to moviedetail page ; complete backend for favorites

--- client/src/pages/moiveDetail.js
+++ client/src/pages/moiveDetail.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { fetchMovieDetails } from '../services/tmdbService'
+import { fetchMovieDetails } from '../services/tmdbService.js';
+import FavoriteButton from '../components/FavoriteButton.js';
+import './movieDetail.css';
 
 const MovieDetail = () => {
   const { id } = useParams(); 
@@ -26,15 +28,20 @@ const MovieDetail = () => {
   }
 
   return (
-    <div>
+    <div className="movie-detail">
+    <div className="container">
       <h1>{movie.title}</h1>
       <img
         src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
         alt={movie.title}
       />
+      {/* Add the FavoriteButton here */}
+      <FavoriteButton movieId={movie.id} />
       <p><strong>Overview:</strong> {movie.overview}</p>
       <p><strong>Release Date:</strong> {movie.release_date}</p>
       <p><strong>Rating:</strong> ⭐ {movie.vote_average}</p>
+       
+    </div>
     </div>
   );
 };
