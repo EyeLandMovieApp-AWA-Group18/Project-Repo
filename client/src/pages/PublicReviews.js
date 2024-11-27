023518af08aac0ad15c2d556c92f696eb3e52496
t3cami00 <t3cami00@students.oamk.fi>
2024-11-27 13:01:07 +0200

    browing reviews phare3

--- client/src/pages/PublicReviews.js
+++ client/src/pages/PublicReviews.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import ReviewTitleSearchBar from "../components/ReviewTitleSearchBar";
 import ReviewStarsSearchBar from "../components/ReviewStarsSearchBar";
 import PublicReviewsItem from "../components/PublicReviewsItem";
-
+import "./PublicReviews.css";
 const PublicReviews = () => {
   const [reviews, setReviews] = useState([]);
   const [filteredReviews, setFilteredReviews] = useState([]);
@@ -50,23 +50,17 @@ const PublicReviews = () => {
   if (error) return <p>Error: {error}</p>;
 
   return (
-    <div>
-      <h1>All Reviews</h1>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          marginBottom: "20px",
-        }}
-      >
+    <div className="public-reviews">
+      <h1 className="public-reviews__title">Reviews</h1>
+      <div className="public-reviews__search-bar">
         <ReviewTitleSearchBar onTitleChange={(title) => handleSearch(title)} />
         <ReviewStarsSearchBar
           onStarChange={(rating) => handleSearch("", rating)}
         />
       </div>
-      <ul style={{ listStyle: "none", padding: 0 }}>
+      <ul className="public-reviews__list">
         {filteredReviews.map((review) => (
-          <li key={review.id} style={{ marginBottom: "20px" }}>
+          <li key={review.id} className="public-reviews__list-item">
             <PublicReviewsItem review={review} />
           </li>
         ))}
