7ba464674339a1a7b22a9735e672ace4395db37f
t3cami00 <t3cami00@students.oamk.fi>
2024-11-27 13:31:39 +0200

    reviewtitlesearchbar update

--- client/src/components/ReviewTitleSearchBar.js
+++ client/src/components/ReviewTitleSearchBar.js
@@ -4,10 +4,12 @@ import "./ReviewTitleSearchBar.css";
 const ReviewTitleSearchBar = ({ onTitleChange }) => {
   const [title, setTitle] = useState("");
 
-  const handleTitleChange = (event) => {
-    const newTitle = event.target.value;
-    setTitle(newTitle);
-    onTitleChange(newTitle);
+  const handleInputChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSearchClick = () => {
+    onTitleChange(title); // 只有点击按钮时才触发搜索
   };
 
   return (
@@ -15,10 +17,23 @@ const ReviewTitleSearchBar = ({ onTitleChange }) => {
       <input
         type="text"
         value={title}
-        onChange={handleTitleChange}
-        placeholder="Search movie"
+        onChange={handleInputChange}
+        placeholder="Search by movie title"
         className="review-title-search-bar__input"
       />
+      <button
+        onClick={handleSearchClick}
+        className="review-title-search-bar__button"
+        aria-label="Search"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+        >
+          <path d="M10 2a8 8 0 015.75 13.38l4.69 4.69a1 1 0 01-1.42 1.42l-4.69-4.69A8 8 0 1110 2zm0 2a6 6 0 100 12 6 6 0 000-12z" />
+        </svg>
+      </button>
     </div>
   );
 };
