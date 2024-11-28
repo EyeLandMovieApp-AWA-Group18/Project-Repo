de04f0e7f739e07a13be7ade06746c6c80c42ba6
t3cami00 <t3cami00@students.oamk.fi>
2024-11-28 12:26:04 +0200

    updare the show reviewslist

--- client/src/components/ReviewItem.js
+++ client/src/components/ReviewItem.js
@@ -1,10 +1,22 @@
 import React from "react";
 
 const ReviewItem = ({ review, onDeleteReview }) => {
+  const stars = Array.from({ length: 5 }, (_, index) => index + 1);
+
   return (
     <li className="review-item">
       <p className="review-email">{review.user_email}</p>
-      <strong>Rating: {review.rating} ⭐</strong>
+
+      <div className=".star-rating">
+        {stars.map((star) => (
+          <span
+            key={star}
+            className={`star ${review.rating >= star ? "filled" : ""}`}
+          >
+            ★
+          </span>
+        ))}
+      </div>
       <p>{review.review_text}</p>
       <p>
         <em>Submitted on: {new Date(review.created_at).toLocaleString()}</em>
