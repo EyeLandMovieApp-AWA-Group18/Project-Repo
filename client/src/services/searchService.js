5c8346b816bfe6c3f22b767d324dbc7d0b329857
PengfeiLi-OAMK <t3lipe01@students.oamk.fi>
2024-12-01 18:50:02 +0200

    add my groups on profile,fix the group card(member count,view grouppage button), fix the movie filter(year,rating range)

--- client/src/services/searchService.js
+++ client/src/services/searchService.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const API_BASE_URL = 'http://localhost:5000/api';
 
-export const fetchMovies = async (query, page = 1) => {
+export const fetchMovies = async (query, page = 1, filters = {}) => {
   try {
     const response = await axios.get(`${API_BASE_URL}/search`, {
-      params: { query, page },
+      params: { query, page, ...filters },
     });
     return response.data;
   } catch (error) {
