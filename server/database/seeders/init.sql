c3dcc070060fcfc9ad5dfc00be6958bd0144e251
PengfeiLi-OAMK <t3lipe01@students.oamk.fi>
2024-12-08 12:10:49 +0200

    add .env.vault and modify pool for deployment; correct errors in database structure

--- server/database/seeders/init.sql
+++ server/database/seeders/init.sql
@@ -71,8 +71,7 @@ CREATE TABLE groupMovies (
     group_id INTEGER NOT NULL,
     movie_id INTEGER NOT NULL,
     posted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE,
-    FOREIGN KEY (movie_id) REFERENCES films(id) ON DELETE CASCADE
+    FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE
 );
 
 
